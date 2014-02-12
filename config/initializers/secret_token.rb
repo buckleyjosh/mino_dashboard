2e9de863f89e30348a6d50f90b86d0c8112f805b
Josh Buckley <buckleyjosh@gmail.com>
2014-02-12 11:07:14 -0800

    Creating dashboard

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MinoDashboard::Application.config.secret_key_base = '246bd0578f86983c6d08347b6a4b9f9a63f652e3ae92a3a014de346bccc45e295a29b59522bded3ecf0e3da5ce54c609184d9113ac903ffd8e13b82ea75091f1'
